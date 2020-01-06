34f3d96d06083f348e9bad740dbee6d5dbc69546
seoilgun <seoilgun@gmail.com>
2020-01-06 13:00:04 +0900

    custom dataset

--- detectron/datasets/json_dataset_evaluator.py
+++ detectron/datasets/json_dataset_evaluator.py
@@ -43,45 +43,132 @@ class CustomCOCOeval(COCOeval):
             self.params.imgIds = sorted(cocoGt.getImgIds())
             self.params.catIds = sorted(cocoGt.getCatIds())
 
+    def summarize(self):
+        '''
+        Compute and display summary metrics for evaluation results.
+        Note this functin can *only* be applied on the default parameter setting
+        '''
 
-class Params:
-    '''
-    Params for coco evaluation api
-    '''
-    def setDetParams(self):
-        self.imgIds = []
-        self.catIds = []
-        # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        # self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-        self.iouThrs = np.linspace(.90, .95, int(np.round((.95 - .90) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
-        self.maxDets = [1, 10, 100]
-        self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-        self.areaRngLbl = ['all', 'small', 'medium', 'large']
-        self.useCats = 1
+        def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
+            p = self.params
+            iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+            titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+            typeStr = '(AP)' if ap == 1 else '(AR)'
+            iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+                if iouThr is None else '{:0.2f}'.format(iouThr)
 
-    def setKpParams(self):
-        self.imgIds = []
-        self.catIds = []
-        # np.arange causes trouble.  the data point on arange is slightly larger than the true value
-        self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
-        self.maxDets = [20]
-        self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-        self.areaRngLbl = ['all', 'medium', 'large']
-        self.useCats = 1
-        self.kpt_oks_sigmas = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62,.62, 1.07, 1.07, .87, .87, .89, .89])/10.0
+            aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+            if ap == 1:
+                # dimension of precision: [TxRxKxAxM]
+                s = self.eval['precision']
+                # IoU
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, :, aind, mind]
+            else:
+                # dimension of recall: [TxKxAxM]
+                s = self.eval['recall']
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, aind, mind]
+            if len(s[s > -1]) == 0:
+                mean_s = -1
+            else:
+                mean_s = np.mean(s[s > -1])
+            print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
+            return mean_s
 
-    def __init__(self, iouType='segm'):
+        def _summarizeDets():
+            stats = np.zeros((23,))
+            stats[0] = _summarize(1)
+            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
+            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+            stats[3] = _summarize(1, iouThr=.85, maxDets=self.params.maxDets[2])
+            stats[4] = _summarize(1, iouThr=.95, maxDets=self.params.maxDets[2])
+            stats[5] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
+            stats[6] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
+            stats[7] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[8] = _summarize(1, iouThr=.5, areaRng='small', maxDets=self.params.maxDets[2])
+            stats[9] = _summarize(1, iouThr=.5, areaRng='medium', maxDets=self.params.maxDets[2])
+            stats[10] = _summarize(1, iouThr=.5, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[11] = _summarize(1, iouThr=.75, areaRng='small', maxDets=self.params.maxDets[2])
+            stats[12] = _summarize(1, iouThr=.75, areaRng='medium', maxDets=self.params.maxDets[2])
+            stats[13] = _summarize(1, iouThr=.75, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[14] = _summarize(1, iouThr=.95, areaRng='small', maxDets=self.params.maxDets[2])
+            stats[15] = _summarize(1, iouThr=.95, areaRng='medium', maxDets=self.params.maxDets[2])
+            stats[16] = _summarize(1, iouThr=.95, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[17] = _summarize(0, maxDets=self.params.maxDets[0])
+            stats[18] = _summarize(0, maxDets=self.params.maxDets[1])
+            stats[19] = _summarize(0, maxDets=self.params.maxDets[2])
+            stats[20] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
+            stats[21] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
+            stats[22] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            return stats
+
+        def _summarizeKps():
+            stats = np.zeros((10,))
+            stats[0] = _summarize(1, maxDets=20)
+            stats[1] = _summarize(1, maxDets=20, iouThr=.5)
+            stats[2] = _summarize(1, maxDets=20, iouThr=.75)
+            stats[3] = _summarize(1, maxDets=20, areaRng='medium')
+            stats[4] = _summarize(1, maxDets=20, areaRng='large')
+            stats[5] = _summarize(0, maxDets=20)
+            stats[6] = _summarize(0, maxDets=20, iouThr=.5)
+            stats[7] = _summarize(0, maxDets=20, iouThr=.75)
+            stats[8] = _summarize(0, maxDets=20, areaRng='medium')
+            stats[9] = _summarize(0, maxDets=20, areaRng='large')
+            return stats
+
+        if not self.eval:
+            raise Exception('Please run accumulate() first')
+        iouType = self.params.iouType
         if iouType == 'segm' or iouType == 'bbox':
-            self.setDetParams()
+            summarize = _summarizeDets
         elif iouType == 'keypoints':
-            self.setKpParams()
-        else:
-            raise Exception('iouType not supported')
-        self.iouType = iouType
-        # useSegm is deprecated
-        self.useSegm = None
+            summarize = _summarizeKps
+        self.stats = summarize()
+
+# class Params:
+#     '''
+#     Params for coco evaluation api
+#     '''
+#     def setDetParams(self):
+#         self.imgIds = []
+#         self.catIds = []
+#         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
+#         # self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
+#         self.iouThrs = np.linspace(.90, .95, int(np.round((.95 - .90) / .05)) + 1, endpoint=True)
+#         self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
+#         self.maxDets = [1, 10, 100]
+#         self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+#         self.areaRngLbl = ['all', 'small', 'medium', 'large']
+#         self.useCats = 1
+#
+#     def setKpParams(self):
+#         self.imgIds = []
+#         self.catIds = []
+#         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
+#         self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
+#         self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
+#         self.maxDets = [20]
+#         self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+#         self.areaRngLbl = ['all', 'medium', 'large']
+#         self.useCats = 1
+#         self.kpt_oks_sigmas = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62,.62, 1.07, 1.07, .87, .87, .89, .89])/10.0
+#
+#     def __init__(self, iouType='segm'):
+#         if iouType == 'segm' or iouType == 'bbox':
+#             self.setDetParams()
+#         elif iouType == 'keypoints':
+#             self.setKpParams()
+#         else:
+#             raise Exception('iouType not supported')
+#         self.iouType = iouType
+#         # useSegm is deprecated
+#         self.useSegm = None
 
 def evaluate_masks(
     json_dataset,
@@ -256,10 +343,10 @@ def _coco_bbox_results_one_category(json_dataset, boxes, cat_id):
 
 def _do_detection_eval(json_dataset, res_file, output_dir):
     coco_dt = json_dataset.COCO.loadRes(str(res_file))
-    # coco_eval = COCOeval(json_dataset.COCO, coco_dt, 'bbox')
-    coco_eval = CustomCOCOeval(json_dataset.COCO, coco_dt, 'bbox')
-    params = Params(iouType='bbox')
-    coco_eval.set_param(params, json_dataset.COCO)
+    coco_eval = COCOeval(json_dataset.COCO, coco_dt, 'bbox')
+    # coco_eval = CustomCOCOeval(json_dataset.COCO, coco_dt, 'bbox')
+    # params = Params(iouType='bbox')
+    # coco_eval.set_param(params, json_dataset.COCO)
     coco_eval.evaluate()
     coco_eval.accumulate()
     _log_detection_eval_metrics(json_dataset, coco_eval)
@@ -277,10 +364,10 @@ def _log_detection_eval_metrics(json_dataset, coco_eval):
         assert np.isclose(iou_thr, thr)
         return ind
 
-    # IoU_lo_thresh = 0.5
-    # IoU_hi_thresh = 0.95
-    IoU_lo_thresh = 0.9
+    IoU_lo_thresh = 0.5
     IoU_hi_thresh = 0.95
+    # IoU_lo_thresh = 0.9
+    # IoU_hi_thresh = 0.95
     ind_lo = _get_thr_ind(coco_eval, IoU_lo_thresh)
     ind_hi = _get_thr_ind(coco_eval, IoU_hi_thresh)
     # precision has dims (iou, recall, cls, area range, max dets)
